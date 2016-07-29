e8749ef3da049351eba3959c613a31ab903f59fe
toyleksut <toy.bubbletea@gmail.com>
2016-07-29 10:40:26 -0700

    tab to space

--- scripts/flatten.py
+++ scripts/flatten.py
@@ -4,20 +4,20 @@ import sys
 import shutil
 
 def flatten(inDir, outDir):
-	counter = 1;
+  counter = 1;
 
-	for d in os.listdir(inDir):
-		subDir = os.path.join(inDir, d)		
-		for root, dirnames, filenames in os.walk(subDir):
-			for f in filenames:
-				srcFile = os.path.join(inDir, d, f)
-				desFile = os.path.join(outDir, f);
-				shutil.move(srcFile, desFile)
-				print "%d mv %s" % (counter, f)
-				counter += 1
+  for d in os.listdir(inDir):
+    subDir = os.path.join(inDir, d)		
+    for root, dirnames, filenames in os.walk(subDir):
+      for f in filenames:
+        srcFile = os.path.join(inDir, d, f)
+        desFile = os.path.join(outDir, f);
+        shutil.move(srcFile, desFile)
+        print "%d mv %s" % (counter, f)
+        counter += 1
 
 	
 if __name__ == "__main__":
-	flatten(sys.argv[1], sys.argv[2])
+  flatten(sys.argv[1], sys.argv[2])
 
     
